774be0b0796d9379c7c5ec619d584206e9c13b35
rohguentak <rhgt78@gmail.com>
2019-07-20 22:14:35 +0900

    make method of comparing name

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -9,10 +9,10 @@ class DirtySample {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!compare_name(items[i], "Aged Brie")
+                    && !compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!compare_name(items[i], "Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,14 +20,14 @@ class DirtySample {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < 50) { // 맨위 조건하고 겹치므로 삭제
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
+                        if (items[i].sellIn < 6) { //위 조건하고 동일 if문 전체삭제가능
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -36,27 +36,31 @@ class DirtySample {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!compare_name(items[i], "Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!compare_name(items[i], "Aged Brie")) {
+                    if (!compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!compare_name(items[i], "Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
-                } else {
+                } else { // selling이 0보다 작고 이름이 aged brie 이고 퀄리티가 50보다작으면 퀄리티늘림
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean compare_name(Item item, String nametocompare) {
+        return item.name.equals(nametocompare);
     }
 }
