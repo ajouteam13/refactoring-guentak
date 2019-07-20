14077eb39ebadfd6a9f4a4808ef19d4389b0c494
rohguentak <rhgt78@gmail.com>
2019-07-20 23:00:57 +0900

    make increaseQuality method

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -10,27 +10,24 @@ class DirtySample {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!compare_name(items[i], "Aged Brie")
-                    && !compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
+            if (!compare_name(items[i], "Aged Brie") && !compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert"))
+            {
                 DecreaseQualityWhenQualityisPositiveandnameisnotSulfuras(items[i]);
-            } else {
+            }
+            else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
+                    increaseQuality(i);
                     if (compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                                items[i].quality = items[i].quality + 1;
+                            increaseQuality(i);
                         }
-
                         if (items[i].sellIn < 6) { //위 조건하고 동일 if문 전체삭제가능
-                                items[i].quality = items[i].quality + 1;
+                            increaseQuality(i);
                         }
                     }
                 }
             }
-
             updateSellln.updateSellln(items[i]);
-
             if (items[i].sellIn < 0) {
                 if (!compare_name(items[i], "Aged Brie")) {
                     if (!compare_name(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
@@ -40,11 +37,15 @@ class DirtySample {
                     }
                 } else { // selling이 0보다 작고 이름이 aged brie 이고 퀄리티가 50보다작으면 퀄리티늘림
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        increaseQuality(i);
                     }
                 }
             }
         }
+    }
+
+    private void increaseQuality(int i) {
+        items[i].quality = items[i].quality + 1;
     }
 
     private void DecreaseQualityWhenQualityisPositiveandnameisnotSulfuras(Item item) {
